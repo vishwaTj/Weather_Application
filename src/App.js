e3a21741e232002602bdac8e7f04b825db5d0073
Tj_ <tjvishwa88@gmail.com>
2023-04-02 11:33:57 +0530

    Iput set input detection is also set

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
-// import * as mdb from 'mdb-ui-kit';
-import { Input } from 'mdb-ui-kit';
+import React, { useState } from 'react';
+
 import "./index.css";
 
 const App = () => {
-    let arr = ["Cairo","Jaipur","Kolkata"];
+    var arr = ["Cairo","Jaipur","Kolkata"];
+  
+    const [city,setCity] = useState('');
+
+    const handleChange = (e)=>{
+        setCity(e.target.value);
+    }
+
+    const handleSubmit=(e)=>{
+       e.preventDefault(); 
+        if(arr.includes(city)){
+          console.log("Hello");}
+        else{
+            console.log("Array not found " + city);
+        }
+    }
+
+    console.log(city)
+
   return (
     <div className='Body'>
       <div className='Input'>
         <h1>Weather App</h1>
-        <div className="input-group rounded input-block">
-            <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-            <span className="input-group-text border-0" id="search-addon">
+        <div className="input-block">
+          <form onSubmit={handleSubmit}>  
+            <input type="search" value={city} placeholder="Search" onChange={handleChange} />
+             {city.length < 3 && 'City name must be longer than 2 letters'}
+          </form> 
+          <div className='search-icon'>
                 <i className="fas fa-search"></i>
-            </span>
+            </div>
         </div>
       </div>
     </div>
